0bfab3ecf7f1eaadee6543ebee80a1064c50acbf
paulooliveira152012 <paulooliveira152012@gmail.com>
2023-05-05 19:23:19 -0400

    nothing is being displayed, not even error messages

--- src/components/loginPage/right.js
+++ src/components/loginPage/right.js
@@ -1,20 +1,61 @@
 import React from "react";
 import './style.css';
 
-function Right() {
-    return (
-        <div className="right">
-            <form>
-                <input type="Text" placeholder="Email or phone number"></input>
-                <input type="Password" placeholder="Password"></input>
-                <button id="login">Log In</button>
-                <a href="">Forgot password?</a>
-                <div className="line"></div>
-                <button id="signup">Create new account</button>
-            </form>
-            <p><span>Create a Page </span> for a celebrity, brand or business.</p>
-        </div>
-    )
+function RegistrationForm() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const response = await fetch('/api/register', {
+      method: 'POST',
+      body: formData
+    });
+    if (response.ok) {
+      // Handle successful registration
+    } else {
+      // Handle failed registration
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" placeholder="Name" />
+      <input type="text" name="username" placeholder="Username" />
+      <input type="email" name="email" placeholder="Email" />
+      <input type="password" name="password" placeholder="Password" />
+      <button type="submit">Create Account</button>
+    </form>
+  );
 }
 
-export default Right
+function Right() {
+    return (
+      <div className="right">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Email or phone number"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button id="login">Log In</button>
+          <a href="">Forgot password?</a>
+          <div className="line"></div>
+          <button id="signup" type="submit">
+            Create new account
+          </button>
+        </form>
+        <p>
+          <span>Create a Page </span> for a celebrity, brand or business.
+        </p>
+      </div>
+    );
+  }
+  
+
+export default Right;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import loginPage from './login.html'
-// import loginPage from './'
 import Left from './components/loginPage/left'
 import Right from './components/loginPage/right'
 import Footer from './components/loginPage/footer';
 
-function MyApp() {
-  return (
-      <><main>
-      <div className='mainContainer'>
-        <Left />
-        <Right />
-      </div>
+function RegistrationForm() {
+  const [email, setEmail] = useState(''); //email
+  const [password, setPassword] = useState(''); //password
 
-    </main>
-    <Footer />
-    
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    if (response.ok) {
+      console.log("success")
+    } else {
+      console.log(Error)
+    }
+  };
+
+  return (
+    <>
+      <main>
+        <div className='mainContainer'>
+          <Left />
+          <Right
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            handleSubmit={handleSubmit}
+          />
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<RegistrationForm />, document.getElementById('root'));
