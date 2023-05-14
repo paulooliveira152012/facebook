58cb66d5a7206ae4cc17b5ebfefe24cb0b5aadd4
paulooliveira152012 <paulooliveira152012@gmail.com>
2023-05-14 18:10:51 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,50 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+// import loginPage from './login.html'
+// import loginPage from './'
 import Left from './components/loginPage/left'
 import Right from './components/loginPage/right'
 import Footer from './components/loginPage/footer';
 
-function RegistrationForm() {
-  const [email, setEmail] = useState(''); //email
-  const [password, setPassword] = useState(''); //password
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const response = await fetch('/api/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    });
-
-    if (response.ok) {
-      console.log("success")
-    } else {
-      console.log(Error)
-    }
-  };
-
+function MyApp() {
   return (
-    <>
-      <main>
-        <div className='mainContainer'>
-          <Left />
-          <Right
-            email={email}
-            setEmail={setEmail}
-            password={password}
-            setPassword={setPassword}
-            handleSubmit={handleSubmit}
-          />
-        </div>
-      </main>
-      <Footer />
+      <><main>
+      <div className='mainContainer'>
+        <Left />
+        <Right />
+      </div>
+
+    </main>
+    <Footer />
+    
     </>
   );
 }
 
-ReactDOM.render(<RegistrationForm />, document.getElementById('root'));
+ReactDOM.render(<MyApp />, document.getElementById('root'));
